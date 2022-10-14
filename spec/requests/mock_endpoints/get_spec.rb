372d453871639a6297aa047bc143a37e1d63a3e2
PRATIK DHARAMDASANI <pratik@commutatus.com>
2022-10-14 17:06:06 +0530

    test: add tests for GET endpoint

---
+++ spec/requests/mock_endpoints/get_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'MockEndpoints', type: :request do
+  describe '.all' do
+    let!(:path) { endpoints_path }
+    let!(:json_api_accept_header) { {"Accept": "application/vnd.api+json"} }
+    let!(:post_params) { sample_post_params }
+    let!(:post_headers) { json_api_headers }
+
+    context "without available mock endpoints" do
+      before do
+        get path, headers: json_api_accept_header
+      end
+
+      it "will return status 'ok'" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "will return an empty array" do
+        expect(response_body).to match_array([])
+      end
+    end
+
+    context "with available mock endpoints" do
+      before do
+        post path, params: post_params, headers: post_headers
+        get path, headers: json_api_accept_header
+      end
+      
+      it "will return status 'ok'" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "will return a non-empty array" do
+        expect(response_body).to be_an_instance_of(Array)
+        expect(response_body).to_not be_empty
+      end
+    end
+  end
+end
